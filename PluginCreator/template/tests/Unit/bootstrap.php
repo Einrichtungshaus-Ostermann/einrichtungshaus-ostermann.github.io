8ca813b8236f6200b0ff1963744cf5e5760c7296
Tim Windelschmidt <fionera@fionera.de>
2018-11-16 12:18:30 +0100

    Fix Unit test Bootstrap

--- PluginCreator/template/tests/Unit/bootstrap.php
+++ PluginCreator/template/tests/Unit/bootstrap.php
@@ -2,6 +2,6 @@
 require __DIR__ . '/../../../../../vendor/autoload.php';
 
 $loader = new \Composer\Autoload\ClassLoader();
-$loader->addPsr4('OstArticleAssemblySurcharge\\', __DIR__ . '/../../');
+$loader->addPsr4('NAMESPACE_NAME\\', __DIR__ . '/../../');
 
 $loader->register();
